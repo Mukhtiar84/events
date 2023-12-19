11feff1cabcdb0238682427b3842a1e1bd6874bd
Mukhtiar84 <mukhtiaralikmc@gmail.com>
2023-12-19 14:27:26 +0500

    events

---
+++ events.js
@@ -0,0 +1,32 @@
+function mycode(){
+    alert('Hello i am an events response')
+}
+function mycode(){
+    document.write('Hello i am an events response')
+}
+
+
+
+function adding(a,b){
+    document.write("Sum of the two numbers is <br>", a+b)
+}
+function adding(a,b){
+    document.write(`Sum of the two numbers ${a} and ${b} is <br> ${a+b}`)
+}
+function differencing(a,b){
+        document.write(`difference of the two numbers ${a} and ${b} is <br> ${a-b}`)
+        
+}       
+function multiplying(a,b){
+            document.write(`Product of the two numbers ${a} and ${b} is <br> ${a*b}`)
+        }
+    
+        function findSum(arr){
+    let sum = 0
+    for(a=0;a<arr.length;a++){
+        sum +=arr[a]
+
+    }
+        document.write(`The sum of array ${arr} is ${sum}`)
+    }
+        
